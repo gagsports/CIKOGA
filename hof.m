6e5bfc8b8fe73ba5943e27245ea718f1017fc778
GagSports <37270634+gagsports@users.noreply.github.com>
2022-04-20 23:21:21 -0500

    Create hof.m

---
+++ hof.m
@@ -0,0 +1,29 @@
+# CIKOGA Hall of Fame
+
+## Major Records & Winners
+
+* Career Grand Slam: B. Chung and H. Yim in 2021
+* Most major wins: 8 by H. Yim
+* Most consecutive major wins: 3 by K. Choo in 2015
+* The lowest singe round record: -2 by B. Chung in 2017 US Open
+* The lowest 36 hole record: Even par by B. Chung in 2017 US Open
+* The youngest major winner: J. Park in 2017 The Open at the age of 25
+* The oldest major winner: J. Ko (2020 US Open) and K. Choo (2019 US Open) at the age of 58
+
+| Year	| The Open    | US Open     |  Championship | Masters      |
+|-------|-------------|-------------|---------------|--------------|
+| 2021	| H. Yim (8)	| S. Park (1)	| J. Shin (1)	  | B. Chung (5) |
+| 2020	| J. Son (2)	| J. Ko (2)   | cancelled	    | cancelled    |
+| 2019	| J. Choi (1)	| K. Choo (4)	| H. Yim (7)	  | H. Yim (6)   |
+| 2018	| B. Chung (4)|	H. Yim (5)	| J. Son (1)	  | H. Yim (4)   |
+| 2017	| J. Park (1)	| B. Chung (2)| B. Chung (3)	| J. Ko (1)    |
+| 2016	| B. Chung (1)| H. Yim (2)  | H. Yim (3)    | H. Yim (1)   |
+| 2015	| K. Choo (2) | K. Choo (1) | K. Choo (3)   |              |
+
+
+## Club Eagle
+
+|Name	 | Tournament	  | Course	  | Hole        |
+|------|--------------|-----------|-------------|
+|H. Yim| 2015 US Open	| Brookhill	| #6 (PAR 5)  |
+|J. Ko | 2017 US Open	| Scoville	| #18 (PAR 4) |
